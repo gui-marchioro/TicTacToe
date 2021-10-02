34bcf0803ae6659e9805b1f7393731c0b77d2150
gui-marchioro <89101832+gui-marchioro@users.noreply.github.com>
2021-10-02 14:31:17 -0300

    Add files via upload

---
+++ TicTacToe_Console/TicTacToe_Console/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_Console
+{
+    class Player
+    {
+        private string name;
+        private bool isX;
+
+        public string Name 
+        {
+            get
+            { 
+                return name;
+            }
+        }
+
+        public bool IsX
+        {
+            get { return isX; }
+        }
+
+
+        public Player(string name, bool isX)
+        {
+            this.name = name;
+            this.isX = isX;
+        }
+    }
+}
---
+++ TicTacToe_Console/TicTacToe_Console/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace TicTacToe_Console
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            Player player1 = new Player("Player 1",true);
+            Player player2 = new Player("Player 2",false);
+            int turn = 0;
+            int place = 0;
+            bool wantToPlay = true;
+            string decision = null;
+
+            while (wantToPlay)
+            {
+                Console.Clear();
+                while (board.IsGameEnded() == false && turn < 9)
+                {
+                    turn++;
+                    Console.WriteLine("Current turn: {0}\n", turn);
+                    board.Print();
+                    if (turn % 2 == 0)
+                    {
+                        Console.WriteLine("\n{0} please select where you want to play", player2.Name);
+                        try
+                        {
+                            place = int.Parse(Console.ReadLine());
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Please enter a number");
+                        }
+                        while (!board.SetPositions(place, player2.IsX))
+                        {
+                            Console.WriteLine("\n Invalid position, please choice another:");
+                            place = int.Parse(Console.ReadLine());
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n{0} please select where you want to play", player1.Name);
+                        try
+                        {
+                            place = int.Parse(Console.ReadLine());
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Please enter a number");
+                        }
+                        while (!board.SetPositions(place, player1.IsX))
+                        {
+                            Console.WriteLine("\n Invalid position, please choice another:");
+                            place = int.Parse(Console.ReadLine());
+                        }
+                    }
+                    Console.Clear();
+                }
+
+                Console.WriteLine("\nEND GAME\n");
+
+                if (board.IsGameEnded() == false)
+                {
+                    Console.WriteLine("TIE GAME\n");
+                }
+                else
+                {
+                    if (turn % 2 == 0)
+                    {
+                        Console.WriteLine("THE WINNER IS: {0}\n", player2.Name);
+                    }
+                    else
+                    {
+                        Console.WriteLine("THE WINNER IS: {0}\n", player1.Name);
+                    }
+                }
+
+                Console.WriteLine("TURNS PLAYED: {0}", turn);
+                board.Print();
+                Console.WriteLine("Want to play again? (Y/N)");
+                decision = Console.ReadLine();
+                if(decision != "Y" && decision != "y")
+                {
+                    Console.WriteLine(decision);
+                    wantToPlay = false;
+                }
+                board = new Board();
+                turn = 0;
+            }
+
+        }
+    }
+}
